a199afc29a58858d908b2c734f4343752f72a933
Marcelo Rocha <contato@omarcelo.com.br>
2020-10-30 12:06:51 -0300

    Update config

--- app/settings.php
+++ app/settings.php
@@ -1,8 +1,11 @@
 <?php
 declare(strict_types=1);
 
+use Cake\Core\Configure;
 use DI\ContainerBuilder;
 use Monolog\Logger;
+
+Configure::write('App.namespace', 'App');
 
 return function (ContainerBuilder $containerBuilder) {
     // Global Settings Object
@@ -11,9 +14,18 @@ return function (ContainerBuilder $containerBuilder) {
             'displayErrorDetails' => true, // Should be set to false in production
             'logger' => [
                 'name' => 'slim-app',
-                'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+                'path' => __DIR__ . '/../logs/app.log',
                 'level' => Logger::DEBUG,
             ],
+            'database' => [
+                'className' => \Cake\Database\Connection::class,
+                'driver' => \Cake\Database\Driver\Mysql::class,
+                'database' => 'my_db',
+                'username' => 'root',
+                'password' => 'secret',
+                'host' => 'mysql',
+            ]
         ],
     ]);
 };
+
